00579e6f6be3b3a1c9350980b726310f7ab5b620
gomezfernando1995 <gomezfernando1995@gmail.com>
2022-11-04 17:08:04 -0300

    2do parcial agregado

---
+++ 2do Cuatri/2doPacial/CalculadoraDePromedio/src/Alumno.java
@@ -0,0 +1,99 @@
+
+public class Alumno {
+
+    //------------------------ ATRIBUTOS DEL ALUMNO----------------
+    private int legajo;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String materia;
+    private int nota;
+
+    //----------------------- METODOS DE ALUMNO ----------------
+    
+    /**
+     * @return the legajo
+     */
+    public int getLegajo() {
+        return legajo;
+    }
+
+    /**
+     * @param legajo the legajo to set
+     */
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the materia
+     */
+    public String getMateria() {
+        return materia;
+    }
+
+    /**
+     * @param materia the materia to set
+     */
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    /**
+     * @return the nota
+     */
+    public int getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the nota to set
+     */
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+    
+   
+}
